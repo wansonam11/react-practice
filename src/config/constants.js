d48c65cc13c2055572f9c7f031beb139480cf4e4
wansonam11 <sosen7874@naver.com>
2024-04-08 19:27:15 +0900

    change2

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = (process.env.NODE_ENV = "production"
+export const API_URL = (process.env.NODE_ENV === "production"
   ? "https://grab-market-server-harrypotter.fly.dev"
   : "http://localhost:8080");
